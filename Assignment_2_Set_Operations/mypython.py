6ef282beaac3707d061f6fbd2351a0a6862dd609
124308134+LazyKoalaMan@users.noreply.github.com <tngo441432@gmail.com>
2024-03-29 19:15:59 -0500

    update

--- Assignment_2_Set_Operations/mypython.py
+++ Assignment_2_Set_Operations/mypython.py
@@ -40,82 +40,6 @@ def deletion(listX, elemToCheck, numOccurrences, size) :
         else :
             return deletion(listX, elemToCheck, numOccurrences, size - 1) + [listX[size - 1]]
 
-def duplicateRemover(listX, size) :
-    '''note why I'm using 1'''
-    if size < 1 :
-        return listX
-    else : 
-        numOccurrences = occurrenceCounter(listX, listX[size - 1], count_elements_recursive(listX))
-        newList = deletion(listX, listX[size - 1], numOccurrences, count_elements_recursive(listX))
-        #if the list hasn't been reduced, then I need to reduce the passed in size by one
-        #otherwise, I shouldn't
-        if (count_elements_recursive(listX) == count_elements_recursive(newList)) :
-            return duplicateRemover(newList, size - 1)
-        else :
-            size = count_elements_recursive(newList)
-            return duplicateRemover(newList, size)
-
-def bubble_sort(ar):
-
-    # Base Case: If array
-    # contains a single element
-    if count_elements_recursive(ar) <= 1:
-        return ar
-
-    # Base Case: If array
-    # contains two elements
-    if count_elements_recursive(ar) == 2:
-        return ar if ar[0] < ar[1] else [ar[1], ar[0]]
-
-    # Store the first two elements
-    # of the list in variables a and b
-    a, b = ar[0], ar[1]
-
-    # Store remaining elements
-    # in the list bs
-    bs = ar[2:]
-
-    # Store the list after
-    # each recursive call 
-    res = []
-
-    # If a < b 
-    if a < b:
-        res = [a] + bubble_sort([b] + bs)
-
-    # Otherwise, if b >= a
-    else:
-        res = [b] + bubble_sort([a] + bs)
-
-    # Recursively call for the list
-    # less than the last element and
-    # and return the newly formed list
-    return bubble_sort(res[:-1]) + res[-1:]
- #//https://www.geeksforgeeks.org/sort-an-array-using-bubble-sort-without-using-loops/
-
-def add_brackets(line, index=0, stack=[]):
-    if index >= len(line):
-        return ""
-    if line[index] == '(':
-        stack.append('(')
-    elif line[index] == ')':
-        if stack:
-            stack.pop()
-    elif line[index] == ',' and stack:
-        return ',' + add_brackets(line, index + 1, stack)
-    elif line[index].isalnum() and not stack:
-        return '(' + line[index] + add_brackets(line, index + 1, ['('])
-    else:
-        return line[index] + add_brackets(line, index + 1, stack)
-
-    return line[index] + add_brackets(line, index + 1, stack)
-
-
-
-    
-
-
-
 
 #===========================================Main=============================================================
 
@@ -129,38 +53,30 @@ with open("example1.txt", "r") as file:
     for line in file:
         if(line[0]!="#"):
             lines1.append(line)
+
         if(funcExist==True):
-            if(line.find("return")==0):
-                func[amount].append(line)
+            if(line.find("return")>0):
                 funcExist=False
             func[amount].append(line)
         else:
+            if(line.find("def")==-1 and funcExist==False and amount>=1):#Handles test case if there is more than 1 return.
+                func[amount].append(line)
+
             start_index=line.find("def")
             end_index=line.find("def")+len("def")
-            if(line[start_index:end_index]=="def"):
+            if(line[start_index:end_index]=="def" and funcExist==False):
                 amount+=1
                 func[amount]=[line]
                 funcExist=True
-        
-    
-    print(func)
 
-    
 
+
+    print(lines1)
     modified_lines = []
     in_function = False
-    
-    for line in file:
-        if line.startswith("def "):
-            in_function = True
-            modified_lines.append(line.strip()[:-1] + "):")
-        elif in_function and line.strip() == "":
-            modified_lines.append(line.strip())
-            modified_lines.append("pass\n")
-            in_function = False
-        else:
-            modified_lines.append(line)
+    amount=1        
 
     with open("output.txt", "w") as result_file:
-        for line in lines1:
-            result_file.write(line+" ")
+        for line in func:
+            result_file.write(str(func[line])+" \n")
+            amount+=1
---
+++ Assignment_2_Set_Operations/pythonInterpreter.c++
@@ -0,0 +1,15 @@
+include <Python.h>
+
+int main() {
+    Py_Initialize();
+
+    // Execute Python code
+    PyRun_SimpleString("print('Hello from Python!')");
+    if (PyErr_Occurred()) {
+        PyErr_Print();
+    }
+    // Finalize Python interpreter
+    Py_Finalize();
+
+    return 0;
+}
